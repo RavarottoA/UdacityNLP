3dc9ffea96ab9d79f547694b775ecf2a2bbc4d22
Alejandro Ravarotto <ravarottoalejandro@gmail.com>
2020-07-20 17:56:52 -0300

    Works so far

--- src/client/index.js
+++ src/client/index.js
@@ -5,7 +5,27 @@ console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+//-----------------------------------------
+document.getElementById("generate").addEventListener("click", performAction);
 
+const axios = require('axios');
+async function getNews(title) {
+    return axios.get('https://api.aylien.com/news/stories', {
+        params: {
+            title: `${title}`
+        },
+        headers: {
+            "X-AYLIEN-NewsAPI-Application-ID": process.env.APP_ID,
+            "X-AYLIEN-NewsAPI-Application-Key": process.env.API_KEY
+        }
+    }).then(r => {
+        let dataSet = r.data.stories; //array of objects
+        return dataSet.map(data => {
+            return data.title
+        })
+    });
+}
+//-----------------------------------------
 export {
     checkForName,
     handleSubmit
--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -21,3 +23,9 @@ app.listen(8081, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+var aylien = require("aylien-news-api");
+var newsapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+ });
